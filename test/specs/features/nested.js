ad67c587f1bc53fc8ae32155eadcffd060af03b9
Chris Bigelow <seebigs@gmail.com>
2018-01-16 13:44:36 -0500

    CB: add an explicit check in our tests for order of execution

--- test/specs/features/nested.js
+++ test/specs/features/nested.js
@@ -1,17 +1,30 @@
+var executionOrder = [];
 
 describe('sponge', function () {
 
+    executionOrder.push(1);
+
     describe('when it gets wet', function (expect) {
 
+        executionOrder.push(2);
+
         describe('grows', function (expect) {
+            executionOrder.push(3);
             expect('larger').toBe(global.wrongValue || 'larger');
         });
 
+        executionOrder.push(4);
+
         describe('does not shrink', function (expect) {
+            executionOrder.push(5);
             expect('same').toBe(global.wrongValue || 'same');
         });
 
         expect('unnested').toBe(global.wrongValue || 'unnested');
+
+        executionOrder.push(6);
+
+        expect(executionOrder).toBe([1,2,3,4,5,6]);
 
     });
 
